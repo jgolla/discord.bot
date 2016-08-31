b0e14fa1188637dd6e9597bde2d9aadbb8438215
jgolla <jason@golla.me>
2016-08-31 17:21:57 -0400

    cleaning up adminactions; adding help command

--- adminactions.js
+++ adminactions.js
@@ -1,94 +1,100 @@
 "use strict";
 
-function adminActions(bot, commandMessage, message) {
+function admin(inputBot) {
+    let bot = inputBot;
+    let botAdminRole = getBotAminRole();
 
-    if(!isUserBotAdmin(bot, message.author)){
-        return;
+    function adminActions(commandMessage, message) {
+
+        if(!isUserBotAdmin(message.author)){
+            return;
+        }
+
+        switch(commandMessage.command) {
+            case '!active': 
+            {
+                bot.sendMessage(message.channel, activeUsers());
+            }
+            break;
+            case '!eval':
+            {
+                // this is bad to do!
+                let output = eval(parsedMessage.body);
+                mybot.sendMessage(message.channel, output);
+            }
+            break;
+            case '!createrole':
+            {
+                let server = getServer();
+                let role = server.roles.get('name', commandMessage.body);
+                if(!role) {
+                    bot.createRole(server, {
+                        name: commandMessage.body		
+                    }).then(() =>  bot.sendMessage(message.channel, `Created role ${commandMessage.body} `));
+                } else {
+                    bot.sendMessage(message.channel, `${commandMessage.body} already exists`);
+                }
+            }
+            break;
+            case '!deleterole':
+            {
+                let server = getServer();
+                let role = server.roles.get('name', commandMessage.body);
+                if(role) {
+                    role.delete().then(() => bot.sendMessage(message.channel, `${commandMessage.body} deleted`));
+                }
+            }
+            break;
+            case '!addtorole':
+            {
+                let params = commandMessage.body.split(':');
+                let server = getServer();
+                let role = server.roles.get('name', params[0]);
+                if(role) {
+                    let user = bot.findUser(params[1]);
+                    bot.addMemberToRole(user, role, (err) => console.log(err)).then(() => bot.sendMessage(message.channel, `${params[1]} added to role ${params[0]}`));
+                } else {
+                    bot.sendMessage(message.channel, `Could not find role ${params[0]}`)
+                }
+            }
+            break;
+            case '!removefromrole':
+            {
+                let params = commandMessage.body.split(':');
+                let server = getServer();
+                let role = server.roles.get('name', params[0]);
+                if(role) {
+                    let user = bot.findUser(params[1]);
+                    bot.removeMemberFromRole(user, role, (err) => console.log(err)).then(() => bot.sendMessage(message.channel, `${params[1]} removed from role ${params[0]}`));
+                } else {
+                    bot.sendMessage(message.channel, `Could not find role ${params[0]}`)
+                }
+            }
+        }
     }
 
-    switch(commandMessage.command) {
-        case '!active': 
-        {
-            bot.sendMessage(message.channel, activeUsers());
-        }
-        break;
-        case '!eval':
-        {
-            // this is bad to do!
-            let output = eval(parsedMessage.body);
-            mybot.sendMessage(message.channel, output);
-        }
-        break;
-        case '!createrole':
-        {
-            let server = getServer(bot);
-            let role = server.roles.get('name', commandMessage.body);
-            if(!role) {
-                bot.createRole(server, {
-                    name: commandMessage.body		
-                }).then(() =>  bot.sendMessage(message.channel, `Created role ${commandMessage.body} `));
-            } else {
-                bot.sendMessage(message.channel, `${commandMessage.body} already exists`);
-            }
-        }
-        break;
-        case '!deleterole':
-        {
-            let server = getServer(bot);
-            let role = server.roles.get('name', commandMessage.body);
-            if(role) {
-                role.delete().then(() => bot.sendMessage(message.channel, `${commandMessage.body} deleted`));
-            }
-        }
-        break;
-        case '!addtorole':
-        {
-            let params = commandMessage.body.split(':');
-            let server = getServer(bot);
-            let role = server.roles.get('name', params[0]);
-            if(role) {
-                let user = bot.findUser(params[1]);
-                bot.addMemberToRole(user, role, (err) => console.log(err)).then(() => bot.sendMessage(message.channel, `${params[1]} added to role ${params[0]}`));
-            } else {
-                bot.sendMessage(message.channel, `Could not find role ${params[0]}`)
-            }
-        }
-        break;
-        case '!removefromrole':
-        {
-            let params = commandMessage.body.split(':');
-            let server = getServer(bot);
-            let role = server.roles.get('name', params[0]);
-            if(role) {
-                let user = bot.findUser(params[1]);
-                bot.removeMemberFromRole(user, role, (err) => console.log(err)).then(() => bot.sendMessage(message.channel, `${params[1]} removed from role ${params[0]}`));
-            } else {
-                bot.sendMessage(message.channel, `Could not find role ${params[0]}`)
-            }
-        }
+    function getServer() {
+        return bot.servers[0];
     }
+
+    function getBotAminRole() {
+        return getServer().roles.get('name', 'botadmin');
+    }
+
+    function isPrivateMessage(channel) {
+        return channel instanceof Discord.PMChannel;
+    }
+
+    function isUserBotAdmin(author) {
+        return author.hasRole(botAdminRole);
+    }
+
+    function activeUsers() {
+        let activeUsers = bot.users.filter(user => user.status === 'online').map(user => user.username);
+        return activeUsers.join(', ');
+    }
+
+    return adminActions;
 }
 
-function getServer(bot) {
-    return bot.servers[0];
-}
-
-function getBotAminRole(bot) {
-    return getServer(bot).roles.get('name', 'botadmin');
-}
-
-function isPrivateMessage(channel) {
-    return channel instanceof Discord.PMChannel;
-}
-
-function isUserBotAdmin(bot, author) {
-    let botAdminRole = getBotAminRole(bot);
-    return author.hasRole(botAdminRole);
-}
-
-function activeUsers(bot) {
-    let activeUsers = bot.users.filter(user => user.status === 'online').map(user => user.username);
-    return activeUsers.join(', ');
-}
-
-module.exports = adminActions;
+module.exports = admin;
--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 var auth = require('./auth.json');
 var commands = require('./commands.json');
 var parseMessage = require('./messageparser.js');
-var adminActions = require('./adminactions.js');
 
 var Discord = require('discord.js');
 var mybot = new Discord.Client();
+
+var admin = require('./adminactions.js');
+var adminActions;
 
 mybot.on('message', function(message) {
 
@@ -30,18 +32,27 @@ mybot.on('message', function(message) {
             let messageToSend = commands[parsedMessage.command].replace('{USER_NAME}', userName);
             mybot.sendMessage(message.channel, messageToSend);
         }
+    } else if(parsedMessage.command === '!help') {
+        let helpList = 'Current commands: ';
+        for(let prop in commands) {
+            helpList += prop + ', ';
+        }
+
+         mybot.sendMessage(message.channel, helpList.substr(0, helpList.length - 2));
     } else {
-        adminActions(mybot, parsedMessage, message);
+        adminActions(parsedMessage, message);
     }
 });
 
 // when the bot is ready
 mybot.on('ready', () => {
     console.log(`Ready to begin! Serving in ${mybot.channels.length} channels`);
-    //botAdminRole = getServer().roles.get('name', 'botadmin');
-
+    
+    // patch bot with utility find user function
     mybot.findUser = (name) => mybot.users.filter(user => user.username.toLowerCase() === name.toLowerCase() || user.mention() === name)[0];
-
+    
+    // wait for the bot to be initialized before setting up the admin actions
+    adminActions = admin(mybot);
 });
 
 mybot.loginWithToken(auth.token);
