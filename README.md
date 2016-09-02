4b6b1ba039da39cc0e35631624c0e5b1c15855bf
jgolla <jason@golla.me>
2016-09-01 21:34:01 -0400

    adding cleverbot

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Discord.bot
 This is my attempt at a Discord bot based off Discord.js.
-
-# Todo
---
+++ plugins/talk.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var Cleverbot = require('cleverbot-node');
+var cleverbot = new Cleverbot();
+var setupResponder = false;
+
+module.exports = function(pluginParameters) {
+
+    if(!setupResponder) {
+        setupResponder = true;
+        pluginParameters.bot.on('message', function(message) {
+            if(message.content.indexOf(pluginParameters.bot.user) !== -1) {
+                let cleanedMessage = message.content.replace(pluginParameters.bot.user, '');
+                Cleverbot.prepare(function(){
+                    cleverbot.write(cleanedMessage, function (response) {
+                        pluginParameters.bot.sendMessage(pluginParameters.message.channel, response.message);
+                    });
+                });
+            }
+        });
+    }
+
+    Cleverbot.prepare(function(){
+        cleverbot.write(pluginParameters.body, function (response) {
+            pluginParameters.bot.sendMessage(pluginParameters.message.channel, response.message);
+        });
+    });
+};
