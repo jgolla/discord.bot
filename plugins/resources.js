f4932ee52399cfaef610f41cc9e1ad6bbb66f852
jgolla <jason@golla.me>
2016-09-03 08:14:23 -0400

    allowing searching for 1 or 2 resources

--- plugins/resources.js
+++ plugins/resources.js
@@ -8,14 +8,28 @@ module.exports = function(pluginParameters) {
         nations = parseFile(__dirname + '/CyberNations_SE_Nation_Stats.txt', Nation);
     }
 
-    let desiredResource1 = pluginParameters.body.toLowerCase();
+    if(!pluginParameters.body) {
+        pluginParameters.bot.sendMessage(pluginParameters.message.channel, 'Usage: !resources <resource> <optional_resource>');
+        return;
+    }
+
+    let desiredResources = pluginParameters.body.toLowerCase().split(' ');
+    let matchBoth = desiredResources.length === 2;
 
     let activeUsers = pluginParameters.bot.users.filter(user => user.status === 'online').map(user => user.mention());
     let entry = pluginParameters.db.find({ nick: { $in: activeUsers } }, (err, docs) => {
         docs.forEach((entry) => {            
             let matchingNation = nations.filter((nation) => nation['Nation ID'] === entry.nationid)[0];
-            if(desiredResource1 === matchingNation['Resource 1'].toLowerCase() || desiredResource1 === matchingNation['Resource 2'].toLowerCase()) {
-                pluginParameters.bot.sendMessage(pluginParameters.message.channel, pluginParameters.message.author + ' you could trade with ' + entry.nick);
+
+            if(!matchBoth) {
+                if(desiredResources[0] === matchingNation['Resource 1'].toLowerCase() || desiredResources[0] === matchingNation['Resource 2'].toLowerCase()) {
+                    pluginParameters.bot.sendMessage(pluginParameters.message.channel, pluginParameters.message.author + ' you could trade with ' + entry.nick);
+                }
+            } else {
+                if((desiredResources[0] === matchingNation['Resource 1'].toLowerCase() || desiredResources[0] === matchingNation['Resource 2'].toLowerCase()) &&
+                (desiredResources[1] === matchingNation['Resource 1'].toLowerCase() || desiredResources[1] === matchingNation['Resource 2'].toLowerCase())) {
+                    pluginParameters.bot.sendMessage(pluginParameters.message.channel, pluginParameters.message.author + ' you could trade with ' + entry.nick);
+                }
             }
         });
     });
