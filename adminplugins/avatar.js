1a2a55da6fb2532f4a439d9a88aa397b4baecb65
jgolla <jason@golla.me>
2016-09-10 15:11:41 -0400

    adding avatar setting code

---
+++ adminplugins/avatar.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let http = require('http');
+let https = require('https');
+
+function action(pluginParameters) {
+    if(pluginParameters.body) {
+        let request = http;
+        if(pluginParameters.body.startsWith('https')) {
+            request = https;
+        }
+
+        request.get(pluginParameters.body, function(response) {
+
+            // Continuously update stream with data
+            let data = [];
+            response.on('data', (d) => {
+                d.forEach((item) => data = data.concat(item));
+            });
+
+            response.on('end', function() {
+                let image = new Buffer(data, 'binary');
+                // set avatar
+                pluginParameters.bot.user.setAvatar(image)
+                    .then(user => pluginParameters.message.channel.sendMessage('<--- Check out my new look'))
+                    .catch(console.log);
+            });        
+        });  
+    } else {
+        help(pluginParameters);
+    }
+}
+
+function help(pluginParameters) {
+    pluginParameters.message.channel.sendMessage('Usage: `!avatar <URL>`');
+}
+
+module.exports = {
+    action: action,
+    help: help
+};
